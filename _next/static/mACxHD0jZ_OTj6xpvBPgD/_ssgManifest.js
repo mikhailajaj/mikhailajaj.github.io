ea525a6a0cb2d82aee28c02dc26a4f25cadc6ad4
mikhailajaj <mikhailajaj@users.noreply.github.com>
2026-01-15 03:58:25 +0000

    deploy: 3c7cf1abdce857ca0d79dd7df8e5c6b7de2de144

---
+++ _next/static/mACxHD0jZ_OTj6xpvBPgD/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fexpertise\u002F[domain]","\u002Fprojects\u002F[id]","\u002Fwork\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
